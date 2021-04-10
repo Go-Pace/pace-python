b6a56db0d2f5fa52eeb1a179b11e46456c2a2c09
paingha <apaingha@gmail.com>
2021-04-10 15:07:23 -0500

    added some preliminary tests

---
+++ gopace/tests/test_pace.py
@@ -0,0 +1,17 @@
+import requests
+from src.gopace import Pace
+
+class AllTests:
+
+    def TestInit(self):
+        self.p = Pace('private_key')
+        self.p.init()
+        assert self.p.token != ""
+
+    def TestGetFareEstimateAddress(self):
+        resp = self.p.getFareEstimateAddress(data = {})
+        assert resp.statusCode == 200
+
+    def TestGetFareEstimateCoordinate(self):
+        resp = self.p.getFareEstimateCoordinate(data = {})
+        assert resp.statusCode == 200
